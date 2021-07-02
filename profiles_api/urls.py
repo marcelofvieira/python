3b50b2ab3a4e6d48cbbab5052d15ce3877a8608c
marcelofvieira@gmail.com <marcelofvieira@gmail.com>
2021-07-02 20:52:45 -0300

    Implementada autorização através de token

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 from profiles_api import views
 
+
 router = DefaultRouter()
 
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
@@ -11,5 +12,6 @@ router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,10 +4,14 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
+
+
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
@@ -106,3 +110,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('id', 'name', 'email',)
 
+
+class UserLoginApiView (ObtainAuthToken):
+    """creating user token"""
+    
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
