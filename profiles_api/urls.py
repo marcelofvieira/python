c7b3e32b2b61239ac75cc1e6e5992979bef6bc48
marcelofvieira@gmail.com <marcelofvieira@gmail.com>
2021-07-01 23:59:54 -0300

    Adicionando permissao ao endpoint profile

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,7 +5,9 @@ from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
 router = DefaultRouter()
+
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello/', views.HelloApiView.as_view()),
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
 from profiles_api import serializers
-
-
+from profiles_api import models
+from profiles_api import permissions
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
@@ -45,6 +47,8 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete a' object"""
         return Response({'method': 'DELETE'})
+
+
 
 class HelloViewSet(viewsets.ViewSet):
     """Test of the viewset"""
@@ -89,3 +93,13 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy (self, request, pk=None):
         """Destroy a obj"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creanting and updating profiles"""
+
+    serializer_class = serializers.UserProfilerSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)   
+    permission_classes = (permissions.UpdateOwnProfile,)
+
